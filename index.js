60c0bf311b75ad9beccf7bb52f505d9133b62df0
Jacopo Scazzosi <jacoposcazzosi@gmail.com>
2015-02-24 19:15:13 +0000

    sets-up the basics - app, config, mongodb

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('');
+console.log('');
+console.log('    Please use: ');
+console.log('    $ node start.js');
+console.log('');
+console.log('');
---
+++ start.js
@@ -0,0 +1,56 @@
+
+var _        = require('underscore');
+var express  = require('express');
+var mongodb  = require('mongodb');
+var url      = require('url');
+var path     = require('path');
+var http     = require('http');
+var debug    = require('debug')('pramantha:start');
+var async    = require('async');
+
+var config   = require('./config/config.json');
+var defaults = require('./config/defaults.json');
+
+_.defaults(config, defaults);
+
+var app    = process.app = express();
+var server = http.createServer(app);
+
+async.series(
+  [
+
+    function(cbSeries) {
+      return mongodb.MongoClient.connect(config.mongoUrl, function(errConnect, db) {
+        if (errConnect) {
+          return cbSeries(errConnect);
+        }
+        debug('MongoDB\'s client has been initialized.');
+        app.set('mongodb', db);
+        return cbSeries();
+      });
+    }, 
+
+    function(cbSeries) {
+      app.use('/helloworld', require('./routes/helloworld'));
+      debug('All routes have been set-up.');
+      return cbSeries();
+    },
+
+    function(cbSeries) {
+      return server.listen(config.port, config.host, cbSeries);
+    }
+
+  ], 
+
+  function(errSeries) {
+    if (errSeries) {
+      throw errSeries;
+    }
+    console.log('Listening on %s:%s', config.host, config.port);
+    return null;
+  }
+
+);
+
+
+
