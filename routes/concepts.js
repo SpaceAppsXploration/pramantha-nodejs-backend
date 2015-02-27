d9b136324a2bfbb5eec54f0e9ff35fce8b3b381a
Jacopo Scazzosi <jacoposcazzosi@gmail.com>
2015-02-27 13:44:50 +0000

    adds partial support for /ancestor and /children in /concepts

--- routes/concepts.js
+++ routes/concepts.js
@@ -3,35 +3,54 @@ var Router  = require('express').Router;
 var utils   = require('../utils');
 var globals = require('../globals');
 var logging = require('../logging');
+var config  = require('../config');
 
 var router     = Router();
 var mongodb    = process.app.get('mongodb');
 var collection = mongodb.collection('base'); 
 var logger     = logging.createLogger({name: 'concepts', type: 'route'});
 
+// router.get('/', function(req, res, next) {
+//   var query = {
+//     '$or': [
+//       {'@type': globals.RDF_CONCEPT_CLASS_URI},
+//       {'@type': globals.RDF_KEYWORD_CLASS_URI}
+//     ]
+//   };
+//   return collection.find(query, function(errFind, cursor) {
+//     if (errFind) {
+//       return next(errFind);
+//     }
+//     return cursor.toArray(function(errToArray, data) {
+//       if (errToArray) {
+//         return next(errToArray);
+//       }
+//       return res.send(data);
+//     });
+//   });
+// });
+
 router.get('/', function(req, res, next) {
-  var query = {
-    "$or": [
-      {"@type": globals.RDF_CONCEPT_CLASS_URI},
-      {"@type": globals.RDF_KEYWORD_CLASS_URI}
-    ]
-  };
-  return collection.find(query, function(errFind, cursor) {
+  return collection.findOne({"chronos:group": "STI"}, function(errFind, doc) {
     if (errFind) {
       return next(errFind);
     }
-    return cursor.toArray(function(errToArray, data) {
-      if (errToArray) {
-        return next(errToArray);
-      }
-      return res.send(data);
-    });
+    if (doc) {
+      return res.redirect(303, config.baseUrl + '/concepts/' + doc['skos:prefLabel']);
+      // return res.send(doc);  
+    } else {
+      return res.status(404).end();
+    }
   });
 });
 
 router.get('/:label', function(req, res, next) {
   var query = {
-    "skos:prefLabel": utils.normalizeLabel(req.params.label)
+    '$or': [
+      {'@type': globals.RDF_CONCEPT_CLASS_URI},
+      {'@type': globals.RDF_KEYWORD_CLASS_URI}
+    ],
+    'skos:prefLabel': utils.normalizeLabel(req.params.label)
   };
   logger.trace('Looking for skos:prefLabel %s', utils.normalizeLabel(req.params.label));
   return collection.findOne(query, function(errFind, doc) {
@@ -47,26 +66,60 @@ router.get('/:label', function(req, res, next) {
 
 router.get('/:label/ancestor', function(req, res, next) {
   var query = {
-    "skos:prefLabel": utils.normalizeLabel(req.params.label)
+    '$or': [
+      {'@type': globals.RDF_CONCEPT_CLASS_URI},
+      {'@type': globals.RDF_KEYWORD_CLASS_URI}
+    ],
+    'skos:prefLabel': utils.normalizeLabel(req.params.label)
   };
   logger.trace('Looking for skos:prefLabel %s', utils.normalizeLabel(req.params.label));
   return collection.findOne(query, function(errFind, doc) {
-
     if (errFind) {
       return next(errFind);
     }
-
     if (!doc) {
       return res.status(404).end();
     }
-
     return utils.getBroaderConcept(doc, collection, function(errGetBroader, broader) {
       if (errGetBroader) {
         return next(errGetBroader);
       }
-      return res.redirect('http://example.com/' + broader['skos:prefLabel']);
+      if (!broader) {
+        return res.status(404).end();
+      }
+      var location = config.baseUrl + '/concepts/' + encodeURIComponent(broader['skos:prefLabel']);
+      // res.set('Content-Location', location);
+      // res.send(broader);
+      return res.redirect(303, location);
     });
-    
+  });
+});
+
+router.get('/:label/children', function(req, res, next) {
+  var query = {
+    '$or': [
+      {'@type': globals.RDF_CONCEPT_CLASS_URI},
+      {'@type': globals.RDF_KEYWORD_CLASS_URI}
+    ],
+    'skos:prefLabel': utils.normalizeLabel(req.params.label)
+  };
+  logger.trace('Looking for skos:prefLabel %s', utils.normalizeLabel(req.params.label));
+  return collection.findOne(query, function(errFind, doc) {
+    if (errFind) {
+      return next(errFind);
+    }
+    if (!doc) {
+      return res.status(404).end();
+    }
+    return utils.getNarrowerConcepts(doc, collection, function(errGetBroader, narrower) {
+      if (errGetBroader) {
+        return next(errGetBroader);
+      }
+      if (!narrower) {
+        return res.status(404).end();
+      }
+      return res.send(narrower);
+    });
   });
 });
 
--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 
 var ObjectID = require('mongodb').ObjectID;
+var async    = require('async');
 
 function normalizeLabel(label) {
   if (typeof(label) != 'string') {
@@ -17,6 +18,7 @@ module.exports.normalizeLabel = normalizeLabel;
 function getBroaderConcept(concept, collection, cb) {
   switch(concept['chronos:group']) {
     case 'divisions': 
+    case 'subjects':
       if (typeof(concept['skos:broader']) === 'object') {
         return collection.findOne({'_id': concept['skos:broader']._id}, cb);
       } else {
@@ -27,7 +29,40 @@ function getBroaderConcept(concept, collection, cb) {
       return cb(new Error('Unsupported'));
     break;
   }
-  
 }
 
-module.exports.getBroaderConcept = getBroaderConcept;
+function getNarrowerConcepts(concept, collection, cb) {
+  switch(concept['chronos:group']) {
+    case 'divisions': 
+    case 'STI':
+      if (Array.isArray(concept['skos:narrower'])) {
+        var query = {
+          _id: {
+            '$in': concept['skos:narrower'].map(function(item) {
+              return item._id;
+            })
+          }
+        }
+        return collection.find(query, function(errFind, cursor) {
+          if (errFind) {
+            return cb(errFind);
+          }
+          return cursor.toArray(function(errToArray, data) {
+            if (errToArray) {
+              return cb(errToArray);
+            }
+            return cb(null, data);
+          });
+        });
+      } else {
+        return cb();
+      }
+    break;
+    default: 
+      return cb(new Error('Unsupported'));
+    break;
+  }
+}
+
+module.exports.getBroaderConcept   = getBroaderConcept;
+module.exports.getNarrowerConcepts = getNarrowerConcepts;
