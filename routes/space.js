676b6f0f01e81e7f9b7c86de3569b088eb3b1f1f
Lorenzo <tunedconsulting@gmail.com>
2015-03-14 16:28:23 +0100

    targets/ and targets/label routes

--- routes/space.js
+++ routes/space.js
@@ -6,6 +6,7 @@ var utils        = require('../utils/utils');
 var ObjectID     = require('mongodb').ObjectID;
 var exporterDocs = require('./space/exporter-docs');
 var exporterMiss = require('./space/exporter-missions');
+var exporterTgt  = require('./space/exporter-targets');
 // var parametizer = require('./concepts/parametizer');
 
 module.exports = function(config, opts) {
@@ -140,6 +141,61 @@ module.exports = function(config, opts) {
     });
   });
 
+  router.get('/targets', function(req, res, next) {
+    res.links({'http://www.w3.org/ns/hydra/core#apiDocumentation': config.baseUrl + '/apidocs/concepts.jsonld'});
+    res.links({'http://www.w3.org/ns/json-ld#context': config.baseUrl + '/contexts/concepts.jsonld'});
+    var query = {
+      'chronos:group': 'targets',
+      // 'chronos:relKeyword._id': {'$exists': true}
+    };
+    return collection.find(query, function(errFind, cursor) {
+      if (errFind) {
+        res.sendStatus(500);
+        return logger.error(errFind);
+      }
+      return cursor.toArray(function(errToArray, data) {
+        if (errToArray) {
+          res.sendStatus(500);
+          return logger.error(errToArray);
+        }
+        return exportMiss(data, function(errExport, exported) {
+          if (errExport) {
+            res.sendStatus(500);
+            return logger.error(errExport);
+          }
+          return res.send(exported);
+        });
+      });
+    });
+  });
+
+  router.get('/targets/:label', function(req, res, next) {
+    res.links({'http://www.w3.org/ns/hydra/core#apiDocumentation': config.baseUrl + '/apidocs/concepts.jsonld'});
+    res.links({'http://www.w3.org/ns/json-ld#context': config.baseUrl + '/contexts/concepts.jsonld'});
+    var query = {
+      'chronos:group': 'targets',
+      'skos:altLabel': utils.regexpifyLabel(utils.decodeLabel(req.params.label))
+      // 'chronos:relKeyword._id': {'$exists': true}
+    };
+    return collection.findOne(query, function(errFind, doc) {
+      if (errFind) {
+        res.sendStatus(500);
+        return logger.error(errFind);
+      }
+      if (!doc) {
+        return res.sendStatus(404);
+      }
+      return exportDocs(doc, function(errExport, exported) {
+        if (errExport) {
+          res.sendStatus(500);
+          return logger.error(errExport);
+        }
+        return res.send(exported);
+      });
+    });
+  });
+
   return router;
 
-}
+};
+
---
+++ routes/space/exporter-targets.js
@@ -0,0 +1,55 @@
+/**
+ * 14/03/2015.
+ */
+
+
+var async    = require('async');
+var utils    = require('../../utils/utils');
+var config   = require('../../config');
+var ObjectID = require('mongodb').ObjectID;
+var _        = require('underscore');
+
+module.exports = function(config, opts) {
+
+  var collection = opts.collection;
+  var baseUrl    = config.baseUrl;
+
+  return function exportTargets(docs, cb) {
+
+    var arr  = Array.isArray(docs);
+    docs = arr ? docs : [docs];
+
+    return async.map(docs, function(doc, cbMap) {
+
+      var exported = {}; //_.extend({}, doc);
+
+      var label = doc['skos:prefLabel'];
+      //var keywords = doc['chronos:relKeyword'];
+      var _id      = doc['_id'];
+
+      /*exported['relatedConcepts'] = keywords.map(function(keyword) {
+        var matches = keyword['@id'].match(/http:\/\/api.pramantha.net\/data\/keywords\/(.+)/);
+        return matches ? config.baseUrl + '/concepts/c/' + matches[1] : null;
+      });*/
+
+      exported['dbpedia']    = doc['owl:sameAs'][0]['@value'];
+      exported['url']        = config.baseUrl + '/space/targets/' + utils.encodeLabel(label);
+      exported['label']      = label;
+      //exported['relatedMissions'] = doc['relMission'];
+      exported['abstract']   = doc['chronos:abstract']['@value'];
+      exported['wikiUrl']    = 'http://en.wikipedia.org/wiki/' + doc['chronos:slug'];
+      exported['ontology']   = 'http://ontology.projectchronos.eu/solarsystem/' + label;
+
+
+      return cbMap(null, exported);
+
+    }, function(errMap, docs) {
+      if (errMap) {
+        return cb(errMap);
+      }
+      return cb(null, arr ? docs : docs[0]);
+    });
+
+  }
+
+};
