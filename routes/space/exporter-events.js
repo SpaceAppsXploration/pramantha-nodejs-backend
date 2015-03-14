3584bb9630c95bc1311bcae9bba6781f4b2b9a30
Lorenzo <tunedconsulting@gmail.com>
2015-03-14 17:08:50 +0100

    space/events/

---
+++ routes/space/exporter-events.js
@@ -0,0 +1,54 @@
+/**
+ * on 14/03/2015.
+ */
+
+
+var async    = require('async');
+var utils    = require('../../utils/utils');
+var config   = require('../../config');
+var ObjectID = require('mongodb').ObjectID;
+var _        = require('underscore');
+
+module.exports = function(config, opts) {
+
+  var collection = opts.collection;
+  var baseUrl    = config.baseUrl;
+
+  return function exportTargets(docs, cb) {
+
+    var arr  = Array.isArray(docs);
+    docs = arr ? docs : [docs];
+
+    return async.map(docs, function(doc, cbMap) {
+
+      var exported = {}; //_.extend({}, doc);
+
+      var label = doc['@id'].slice(-32);
+      //var keywords = doc['chronos:relKeyword'];
+      var _id      = doc['_id'];
+
+      exported['relatedMission'] = missions.map(function(mission) {
+        var matches = mission['@id'].match(/http:\/\/api.pramantha.net\/data\/keywords\/(.+)/);
+        return matches ? config.baseUrl + '/space/missions/' + matches[1] : null;
+      });
+
+      exported['url']        = config.baseUrl + '/space/events/' + label;
+      exported['header']     = doc['chronos:eventHeader'];
+      //exported['relatedMissions'] = doc['relMission'];
+      exported['date']       = doc['chronos:eventDate']['@value'];
+      exported['content']    = doc['chronos:eventContent'];
+      exported['relMission'] = doc['chronos:relMission']['@id'];
+
+
+      return cbMap(null, exported);
+
+    }, function(errMap, docs) {
+      if (errMap) {
+        return cb(errMap);
+      }
+      return cb(null, arr ? docs : docs[0]);
+    });
+
+  }
+
+};
